4b2ea445db4742077f5808006b24a79b0fd3f6c8
aditya-kumbhar <31104730+aditya-kumbhar@users.noreply.github.com>
2017-09-04 23:15:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,10 +13,12 @@ img.onclick = function()
     // var inter = setInterval(moveRight,100);
     var name = "Tai";
 	var i = 0;
+
 	while (i < 2) {
-		bday.innerHTML+=("Happy Birthday to you.<br>");
+	    	var timer = setTimeout(bday.innerHTML+=("Happy Birthday to you.<br>"),1000);
 		i = i + 1;
 	}
+	
 	bday.innerHTML+=("Happy Birthday dear " + name + ",<br>");
 	bday.innerHTML+=("Happy Birthday to you.<br>");  
 }
